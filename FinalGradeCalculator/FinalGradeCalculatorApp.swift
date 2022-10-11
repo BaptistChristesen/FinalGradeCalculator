8ceb7b1066d2345e89d8562b3f56ac4e052acd24
Caden Christesen <BaptistChristesen@gmail.com>
2022-10-11 09:46:36 -0500

    Initial Commit

---
+++ FinalGradeCalculator/FinalGradeCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalGradeCalculatorApp.swift
+//  FinalGradeCalculator
+//
+//  Created by caden christesen on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct FinalGradeCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
